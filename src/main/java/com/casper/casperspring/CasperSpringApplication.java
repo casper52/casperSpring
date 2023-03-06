d3573316056918913b8c157635d4a55479a7efda
gayoungggp <casper_52@naver.com>
2023-03-06 16:30:50 +0900

    spring practice start 23/03/06

---
+++ src/main/java/com/casper/casperspring/CasperSpringApplication.java
@@ -0,0 +1,13 @@
+package com.casper.casperspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasperSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasperSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casper/casperspring/CasperSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casper.casperspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasperSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
